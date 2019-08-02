e4ec4009285ed0d9ae1704f2f4e41b1cdebd6b01
tao.wang <tao.wang@xyroyale.com>
2019-08-02 20:31:55 +0800

    汇总模块修改

--- src/views/modules/overview/data-overview/component/onlineData.vue
+++ src/views/modules/overview/data-overview/component/onlineData.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="overview-row">
         <el-card class="small-card" v-for="(item,index) in overviewList" :key="index">
-        <div class="desc-group">
-          <div class="desc-label">
-            <div class="desc-label-name">当{{datetype==1?'日':(datetype==2?'周':'月')}}{{item.name}}</div>
-            <div class="desc-label-value">{{(index==0||index==2)?Number(item.value).toFixed(0):item.value}}</div>
+          <div class="desc-group">
+            <!-- <div class="desc-label">
+              <div class="desc-label-name">当{{datetype==1?'日':(datetype==2?'周':'月')}}{{item.name}}</div>
+              <div class="desc-label-value">{{(index==0||index==2)?Number(item.value).toFixed(0):item.value}}</div>
+            </div> -->
+            <div class="desc-chart">
+              <inlineChart class="desc-trend" :datetype="{item:item,index:index}"  :chartId="'inline-chart'+index" :datas="item.list"></inlineChart>
+            </div>
           </div>
-          <div class="desc-chart">
-            <inlineChart class="desc-trend" :chartId="'inline-chart'+index" :data="item.list"></inlineChart>
-          </div>
-        </div>
       </el-card>
    </div>
 </template>
 <script>
 import api from 'src/services/api'
-import inlineChart from 'src/components/inline-chart'
+// import inlineChart from 'src/components/inline-chart'
+import inlineChart from 'src/components/online-chart'
 export default {
   name: 'online-data',
   props: ['datetype', 'onlineData'],
@@ -32,11 +33,13 @@ export default {
       let result = []
       if (this.onlineData && this.onlineData.length > 0) {
         for (let index in this.onlineData[0]) {
-          if (index != '统计时间') {
+          if (index != '统计时间') {            
             result.push({
               name: index,
               value: 0,
-              list: []
+              list: [],
+              xAxis:[],
+              tableData:[],
             })
           }
         }
@@ -46,6 +49,9 @@ export default {
         let last = 0
         this.onlineData.forEach((item, index) => {
           res.list.push(Number(item[res.name]))
+          res.xAxis.push(item["统计时间"])
+          res.tableData.push({date:item["统计时间"],data:Number(item[res.name])})
+          // res.xAxis.push(item[res])
           // value += Number(item[res.name])
           value = value >= item[res.name] ? value : Number(item[res.name])
 
@@ -108,7 +114,7 @@ export default {
     flex: 3;
     .desc-trend {
       width: 100%;
-      height: 100%;
+      height: auto;
     }
   }
 }
