e002453eafe04dce1e16f7fef68541dc1653ba97
tao.wang <tao.wang@xyroyale.com>
2019-08-05 17:56:48 +0800

    sum

--- src/views/modules/h5_system/h5-report/h5-detailed-report/index.vue
+++ src/views/modules/h5_system/h5-report/h5-detailed-report/index.vue
@@ -501,13 +501,14 @@ export default {
         // sums[9] = (sums[10] / sums[4]).format(2); //创角成本
         // sums[12] = ((sums[11] / sums[10])*100).format(2) + '%'; //ROI
         // sums[14] = ((sums[13] / sums[10])*100).format(2) + '%'; //分成ROI
-        
+
         sums[6] = ((sums[6] / sums[2])).format(2); //7日LTV
         sums[7] = ((sums[7] / sums[2])).format(2); //14日LTV
         sums[8] = ((sums[8] / sums[2])).format(2); //30日LTV
         sums[9] = ((sums[9] / sums[2])).format(2) + '%';
         sums[10] = ((sums[10] / sums[2])).format(2) + '%';
         sums[11] = ((sums[11] / sums[2])).format(2) + '%';
+        
         return sums;
       },
       addStyle({ row, column, rowIndex, columnIndex }) {
